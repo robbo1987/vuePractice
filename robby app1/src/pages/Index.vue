5907a39586c07a3391e8e4ba92f426809003982d
Robert Albenda <ralbenda@gmail.com>
2022-05-14 18:57:10 -0400

    setting up vue

---
+++ robby app1/src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page padding>
+    <input v-model="message" />
+    <h5>{{ message }}</h5>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return { message: "i love coding!" };
+  },
+};
+</script>
+
+<style></style>
