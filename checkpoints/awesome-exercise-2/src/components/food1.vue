40ad9db5d2dffd12d551dbc18fb927aeed69feeb
Robert Albenda <ralbenda@gmail.com>
2022-05-15 22:39:21 -0400

    further practice

---
+++ checkpoints/awesome-exercise-2/src/components/food1.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card shadow-1">
+    <img :src="food.imageUrl" width="198" height="180" />
+    <div class="card-content">
+      <h1 class="text-primary">{{ food.name }}</h1>
+      <p>{{ food.description }}</p>
+    </div>
+    <p>
+      <small>
+        <b>Delicousness:</b>
+      </small>
+      <b class="text-primary">
+        {{ food.deliciousness }}
+      </b>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["food"]
+};
+</script>
