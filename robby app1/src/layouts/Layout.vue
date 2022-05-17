c73dc707d10e026a3937b975cf2ce468bebbafdb
Robert Albenda <ralbenda@gmail.com>
2022-05-16 21:21:28 -0400

    making progress

---
+++ robby app1/src/layouts/Layout.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label header class="text-grey-8"> Navigation </q-item-label>
+        <q-item clickable>
+          <q-item-section avatar>
+            <q-icon name="list" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>ToDo</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+
+  data() {
+    return {
+      leftDrawerOpen: false,
+    };
+  },
+};
+</script>
---
+++ robby app1/src/pages/PageTodo.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page padding>
+    <p>ToDo Page </p>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style></style>
