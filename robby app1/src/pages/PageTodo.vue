4ed8e345281f9c42706aa4b4fedbbaf8c5fa4e36
Robert Albenda <ralbenda@gmail.com>
2022-05-17 20:28:22 -0400

    making ui progress

--- robby app1/src/pages/PageTodo.vue
+++ robby app1/src/pages/PageTodo.vue
@@ -1,20 +1,48 @@
 <template>
   <q-page class="q-pa-lg">
 
-    <q-list bordered>
+    <q-list 
+      separator
+      bordered>
 
       
       <q-item  
         v-for ="task in tasks"
         :key = "task.id"
         @click="task.completed = !task.completed"
+        class="bg-teal-12"
         v-ripple>
         <q-item-section side top>
           <q-checkbox v-model="task.completed" />
         </q-item-section>
 
         <q-item-section>
-          <q-item-label>{{task.name}}</q-item-label>
+          <q-item-label> {{task.name}}</q-item-label>
+        </q-item-section>
+
+         <q-item-section side>
+           <div class="row">
+            <div class="column">
+           <q-icon 
+           name="event"
+           size="18px"
+           class="q-mr-xs"
+            />
+            </div>
+            <div class="column justify-center">
+          <q-item-label 
+            class="row justify-end"
+            caption>
+            Due Date: {{task.dueDate}}
+            </q-item-label>
+             <q-item-label 
+             class="row justify-end"
+             caption>
+            <small>Complete by Time: {{task.dueTime}}</small>
+            
+            </q-item-label>
+            </div>
+            </div>
         </q-item-section>
       </q-item>
 
@@ -31,17 +59,23 @@ export default {
       { 
         id:1,
         name: "Go Shopping",
-        completed: false
+        completed: false,
+        dueDate:'2022/05/22',
+        dueTime: "8:30 AM"
       },
       { 
         id:2,
         name: "Walk The Dog",
-        completed: false
+        completed: false,
+        dueDate:'2022/05/23',
+        dueTime: "8:45 PM"
       },
       { 
         id:3,
         name: "Practice Coding",
-        completed: false
+        completed: false,
+        dueDate:'2022/05/24',
+        dueTime: "12:30 PM"
       },
 
 
